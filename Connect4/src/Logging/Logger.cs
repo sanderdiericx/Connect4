b82e189d2848bfcc8e2d0d758ef4d56e46e7bc2f
sanderdiericx <sanderdiericx@gmail.com>
2025-09-18 18:50:16 +0200

    Fixed .gitignore so github includes logger

---
+++ Connect4/src/Logging/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Connect4.src.Logs
+{
+    internal static class Logger
+    {
+        private static readonly string FILE_NAME = "logs.txt";
+
+        public static void ClearLogs()
+        {
+            File.WriteAllText(FILE_NAME, "");
+        }
+
+        public static void LogException(Exception e)
+        {
+            string content = $"({DateTime.Now.ToString()}) ERROR: {e.Message}\n{e.StackTrace}\n";
+
+            File.AppendAllText(FILE_NAME, content);
+        }
+
+        public static void LogWarning(string warning)
+        {
+            string content = $"({DateTime.Now.ToString()}) WARNING: {warning}\n";
+
+            File.AppendAllText(FILE_NAME, content);
+        }
+    }
+}
